1090c630d17664346d55ca17bb784a26545a0a20
zuaa@163.com <zuaa@163.com>
2016-11-09 10:54:07 +0800

    修改代码

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
-import Top from './Top';
+
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import Top from './Top';
+// import App from './App';
+  import Top from './Top';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-ReactDOM.render(
-<Top/>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+// <Top/>,
+//   document.getElementById('root')
+// );
+
+ 
+
+ReactDOM.render(<Top/> ,  document.getElementById('root'));
